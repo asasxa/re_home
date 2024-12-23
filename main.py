a7efa426091ea85b56455634c81561a8f8cc5974
asasxa <163177231+asasxa@users.noreply.github.com>
2024-12-23 15:14:17 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,20 @@ from pprint import pprint
 import re
 import csv
 
+def format_name(row):
+    name_items = row[0].split()
+    row[0], row[1], row[2] = name_items[0], name_items[1], name_items[2] if len(name_items) > 2 else ''
+    return row
+
+def format_phone(row)
+    number_phone = (row[-2])
+    number_phone = re.sub(r"^8", '+7', number_phone)
+    number_phone = re.sub(r"[ ()-]", "", number_phone)
+    number_phone = re.sub(r"доб.", " доб. ", number_phone)
+    number_phone = f"{number_phone[:2]} ({number_phone[2:5]}){number_phone[5:8]}-{number_phone[8:10]}-{number_phone[10:]}"
+    row[-2] = number_phone
+    return row
+
 unique_contacts = dict()
 
 with open("phonebook_raw.csv", encoding="utf-8") as f:
@@ -10,19 +24,8 @@ with open("phonebook_raw.csv", encoding="utf-8") as f:
     contacts_list = list(rows)
 
 for row in contacts_list[1:]:
-    name_items = row[0].split()
-    if len(name_items) > 1:
-        row[0] = name_items[0]
-        row[1] = name_items[1]
-        if len(name_items) > 2:
-            row[2] = name_items[2]
-    if row[-2]:
-        number_phone = (row[-2])
-        number_phone = re.sub(r"^8", '+7', number_phone)
-        number_phone = re.sub(r"[ ()-]", "", number_phone)
-        number_phone = re.sub(r"доб.", " доб. ", number_phone)
-        number_phone = f"{number_phone[:2]} ({number_phone[2:5]}){number_phone[5:8]}-{number_phone[8:10]}-{number_phone[10:]}"
-        row[-2] = number_phone
+    format_name(row)
+    format_phone(row)
 
 for row in contacts_list[1:]:
     lastname, firstname, surname = row[0], row[1], row[2]
@@ -36,9 +39,6 @@ for row in contacts_list[1:]:
             if existing_contact[i] == "" and row[i] != "":
                 existing_contact[i] = row[i]
 
-new_contact = [contacts_list[0],]
-[new_contact.append(i) for i in unique_contacts.values()]
-
 with open("phonebook.csv", "w", encoding="utf-8") as f:
     datawriter = csv.writer(f, delimiter=',')
-    datawriter.writerows(new_contact)
+    datawriter.writerows([contacts_list[0]] + list(unique_contacts.values()))
